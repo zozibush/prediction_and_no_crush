9168cbc389f2f1524aa9e240c5adcbc6a1bdef38
zozibush <zozibush88@gmail.com>
2024-04-26 09:49:11 +0900

    Remove coordinate conversion and sensor data filtering in environment.py

--- src/vehicle_node/src/environment.py
+++ src/vehicle_node/src/environment.py
@@ -131,15 +131,8 @@ class Environments(object):
                 sensor_info = self.vehicles[id_].get_measure(self.vehicles)
                 local_lane_info = self.vehicles[id_].get_local_path()
 
-                # 1. convert to global
-                global_sensor_info = self.convert_to_global_sensor(sensor_info, self.vehicles[id_])
-                global_lane_info = self.convert_to_global_path(local_lane_info, self.vehicles[id_])
-
-                # 2. filtering
-                filtered_sensor_info = self.filtering(global_sensor_info)
-
-                # 3. control
-                ax, steer = self.control(self.vehicles[id_], global_lane_info, filtered_sensor_info)
+                # 1. control
+                ax, steer = self.control(self.vehicles[id_], local_lane_info, sensor_info)
 
                 self.vehicles[id_].step_manual(ax, steer)
 
@@ -150,58 +143,6 @@ class Environments(object):
     def respawn(self):
         if len(self.vehicles)<self.min_num_agent:
             self.spawn_agent()
-
-    def convert_to_global_path(self, local_path, vehicle):
-        rotation_mat = np.array([[np.cos(-vehicle.h), -np.sin(-vehicle.h)],
-                                [np.sin(-vehicle.h), np.cos(-vehicle.h)]])
-
-        global_pos = np.matmul(local_path[:,:2], rotation_mat) + np.array([vehicle.x, vehicle.y])
-        global_head = np.arctan2(np.sin(local_path[:,2] + vehicle.h), np.cos(local_path[:,2] + vehicle.h))[:,np.newaxis]
-        global_R = local_path[:,-1][:,np.newaxis]
-
-        return np.concatenate([global_pos, global_head, global_R], axis=-1)
-
-    def convert_to_global_sensor(self, sensors, vehicle):
-        GlobalSensors = []
-        rotation_matrix = np.array([[np.cos(vehicle.h), -np.sin(vehicle.h)],
-                                    [np.sin(vehicle.h), np.cos(vehicle.h)]])
-
-        for sensor in sensors:
-            id_, local_x, local_y, local_h, local_vx, local_vy = sensor
-
-            global_pos = np.dot(rotation_matrix, np.array([local_x, local_y])) + np.array([vehicle.x, vehicle.y])
-            global_vel = np.dot(rotation_matrix, np.array([local_vx, local_vy]))
-
-            global_h = local_h + vehicle.h
-            global_h = np.arctan2(np.sin(global_h), np.cos(global_h))
-
-            GlobalSensors.append([id_, global_pos[0], global_pos[1], global_h, global_vel[0], global_vel[1]])
-
-        return GlobalSensors
-
-    def filtering(self, sensor_info):
-        filtered_sensor_info = []
-        # id에 따라 센서 정보를 분류
-        for sensor in sensor_info:
-            vehicle_id = sensor[0]
-            data = np.array(sensor[1:])
-            if vehicle_id in self.sensors.keys():
-                np.append(self.sensors[vehicle_id], data.reshape(1,5), axis=0)
-            else:
-                self.sensors[vehicle_id] = [data]
-
-        # noise filtering use by moving average filter
-        for key in self.sensors.keys():
-            self.sensors[key] = np.array(self.sensors[key])
-            filtered_sensor_info.append([key] + list(np.mean(self.sensors[key], axis=0)))
-            win_size = min(5, len(self.sensors[key]))
-            filtered_sensor_info[-1][0] = moving_average(self.sensors[key][:,0], win_size)
-            filtered_sensor_info[-1][1] = moving_average(self.sensors[key][:,1], win_size)
-            filtered_sensor_info[-1][2] = moving_average(self.sensors[key][:,2], win_size)
-            filtered_sensor_info[-1][3] = moving_average(self.sensors[key][:,3], win_size)
-            filtered_sensor_info[-1][4] = moving_average(self.sensors[key][:,4], win_size)
-
-        return filtered_sensor_info
 
     def control(self, vehicle, lane_info, sensor_info):
         def get_distance(x1, y1, x2, y2):
